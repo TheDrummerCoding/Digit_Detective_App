557ff2e15e6ee011ab1ae5e0e4440cb9118075e8
Fabio <percufabiopercu@gmail.com>
2021-06-14 15:32:29 +0200

    Pantalla de Logo y Pantalla de Bienvenida

---
+++ lib/WelcomePage.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "avenir"),
+      home: welcomePage(),
+    );
+  }
+}
+
+class welcomePage extends StatefulWidget {
+  const welcomePage({Key key}) : super(key: key);
+
+  @override
+  _welcomePageState createState() => _welcomePageState();
+}
+
+class _welcomePageState extends State<welcomePage> {
+  int currentPage = 0;
+  PageController _pageController =
+      new PageController(initialPage: 0, keepPage: true);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.6,
+                child: PageView(
+                  controller: _pageController,
+                  children: [
+                    onBoardPage("welcomepage1", "Welcome to Digit Detective!",
+                        "You just have to draw a number and artificial intelligence will identify what it is, it's that easy!"),
+                    onBoardPage("welcomepage2", "How It Works?",
+                        "To solve this classification problem we use a very popular algorithm in machine learning: Convolutional Neural Networks"),
+                    onBoardPage("welcomepage3", "The Magic Database",
+                        "We have used the MNIST database. This database contains 60,000 training examples and 10,000 test examples.")
+                  ],
+                  onPageChanged: (value) => {setCurrentPage(value)},
+                ),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(3, (index) => getIndicator(index)))
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(top: 20),
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("asset/image/backgroundWelcome.png"),
+                      fit: BoxFit.fill)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 100),
+                      decoration:
+                          BoxDecoration(color: Colors.white, boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            offset: Offset(0, 9),
+                            blurRadius: 20,
+                            spreadRadius: 3)
+                      ]),
+                      child: Text(
+                        "Get Started!",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  AnimatedContainer getIndicator(int pageNumber) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 100),
+      height: 10,
+      width: (currentPage == pageNumber) ? 20 : 10,
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: (currentPage == pageNumber) ? Colors.black : Colors.grey),
+    );
+  }
+
+  Column onBoardPage(String img, String title, String subtitle) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          height: 280,
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.all(50),
+          decoration: BoxDecoration(
+              image:
+                  DecorationImage(image: AssetImage("asset/image/$img.png"))),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Text(
+            title,
+            style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w600,
+                color: Colors.blueAccent),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+          child: Text(
+            subtitle,
+            style: TextStyle(fontSize: 16, color: Colors.grey),
+            textAlign: TextAlign.center,
+          ),
+        )
+      ],
+    );
+  }
+
+  setCurrentPage(int value) {
+    currentPage = value;
+    setState(() {});
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'WelcomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyLogoPage(),
+    );
+  }
+}
+
+class MyLogoPage extends StatefulWidget {
+  const MyLogoPage({Key key}) : super(key: key);
+
+  @override
+  _MyLogoPageState createState() => _MyLogoPageState();
+}
+
+class _MyLogoPageState extends State<MyLogoPage> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), openWelcomePage);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("asset/image/digitdetectivelogo.png")),
+        ),
+      ),
+    ));
+  }
+
+  void openWelcomePage() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => WelcomePage()));
+  }
+}
